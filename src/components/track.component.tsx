ff8ddf0b0b3518f4fbe67163d32295252547b531
stevebarakat <stevebarakat@gmail.com>
2023-12-22 02:39:45 -0500

    solo toggle button to checkbox;

--- src/components/track.component.tsx
+++ src/components/track.component.tsx
@@ -14,32 +14,34 @@ export const Track = ({ actorRef, index }: TrackProps) => {
   console.log("track", track.track.name);
 
   // const volume = 30;
-  const muted = false;
-  const soloed = false;
   return (
     <div className="track">
       <h3 className="track-number">{track.track.name}</h3>
-      <button
-        className={clsx({
-          ["solo-button"]: true,
-          soloed: soloed,
-          unsoloed: !soloed,
-        })}
-        onClick={(event) => {
-          send({ type: "track.toggleSoloed" });
-          event.currentTarget.blur();
-        }}
-      >
-        <span aria-hidden="true">S</span>
-        <span className="sr-only">Solo track</span>
-      </button>
-      <input
-        type="checkbox"
-        onClick={(e) => {
-          send({ type: "track.toggleMuted", checked: e.currentTarget.checked });
-        }}
-      />
-      <label>M</label>
+      <div className="toggle">
+        {" "}
+        <input
+          type="checkbox"
+          onClick={(e) => {
+            send({
+              type: "track.toggleSoloed",
+              checked: e.currentTarget.checked,
+            });
+          }}
+        />
+        <label htmlFor="">S</label>
+      </div>
+      <div className="toggle">
+        <input
+          type="checkbox"
+          onClick={(e) => {
+            send({
+              type: "track.toggleMuted",
+              checked: e.currentTarget.checked,
+            });
+          }}
+        />
+        <label>M</label>
+      </div>
 
       <div className="volume-number">
         <input
